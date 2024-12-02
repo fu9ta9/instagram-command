e6f62bf0f2056b0c983699a38783ef2d5007d34c
fu9ta9 <66508866+fu9ta9@users.noreply.github.com>
2024-12-02 10:36:23 +0900

    fix: remove all scope

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -16,17 +16,15 @@ export const authOptions: NextAuthOptions = {
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID!,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
-      authorization: {
-        params: {
-          scope: [
-            'email',
-            'public_profile',
-            'instagram_basic',
-            'pages_show_list',
-            'business_management'
-          ].join(',')
-        }
-      }
+      // authorization: {
+      //   params: {
+      //     scope: [
+      //       'email',
+      //       'public_profile',
+      //       'instagram_basic',
+      //     ].join(',')
+      //   }
+      // }
     }),
   ],
     pages: {
