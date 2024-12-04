636fec19455ebc315f118b598092e0f68ea1052e
fu9ta9 <66508866+fu9ta9@users.noreply.github.com>
2024-12-04 20:47:56 +0900

    fix: format normal for fb login

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -7,27 +7,46 @@ import type { Adapter } from "next-auth/adapters";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma) as Adapter,
-  secret:process.env.NEXTAUTH_SECRET,
+  secret:process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
     FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID!,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
+      clientId: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID!,
+      clientSecret: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_SECRET!,
       authorization: {
         params: {
+          display: "page",
+          extras: JSON.stringify({
+            setup: {
+              channel: "IG_API_ONBOARDING"
+            }
+          }),
+          response_type: "token",
           scope: [
-            'email',
-            'public_profile',
             'instagram_basic',
+            'instagram_manage_comments',
+            'pages_show_list',
+            'pages_read_engagement'
           ].join(',')
         }
-      }
+      },
     }),
   ],
-    pages: {
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token
+      }
+      return token
+    },
+    async signIn({ user, account, profile, email, credentials }) {
+      return true
+    },
+  },
+  pages: {
     signIn: '/dashboard',
     error: '/auth/error',
   },
