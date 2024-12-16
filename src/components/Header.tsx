0bc498100451052f92bfc9b3daa527a93c83560b
fu9ta9 <66508866+fu9ta9@users.noreply.github.com>
2024-12-16 22:47:09 +0900

    feat: login header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,7 +20,7 @@ export default function Header() {
           お問い合わせ
         </Link>
         <Button variant="outline" asChild>
-          <Link href="/dashboard">ログイン</Link>
+          <Link href="/login">ログイン</Link>
         </Button>
       </nav>
     </header>
