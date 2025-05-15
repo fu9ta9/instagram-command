2559f43303576a3cc04e080703dca49c39c5578b
fu9ta9 <66508866+fu9ta9@users.noreply.github.com>
2025-05-15 21:33:36 +0900

    feat: SVGアイコンを追加し、レイアウトにファビコンとしてリンク

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default async function RootLayout({
     <html lang="ja" suppressHydrationWarning>
       <head>
         <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />
+        <link rel="icon" type="image/svg+xml" href="/icon.svg" />
       </head>
       <body className={cn(inter.className, "bg-gray-50 w-full min-w-0 max-w-full overflow-x-hidden")}>
         <AuthProvider>
