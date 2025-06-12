027466d25befd950ce14a9ea7f22da8684e532a4
fu9ta9 <66508866+fu9ta9@users.noreply.github.com>
2025-06-12 21:33:42 +0900

    feat: Prismaクライアントのエクスポートをグローバルスコープに統合

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -22,3 +22,5 @@ type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClientSingleton | undefined
 }
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
